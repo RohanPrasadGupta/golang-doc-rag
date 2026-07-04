4fea99b4bdbe680f35addc7d711a341c0861b600
ROHAN PRASAD GUPTA <rohanprasad.g@rvconnex.com>
2026-07-04 22:38:26 +0700

    phase 1.2

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,17 +1,22 @@
 package server
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func NewServer() *chi.Mux {
 	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
 	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Server is running!"))
+		w.Header().Set("Content-Type", "application/json")
+		response := map[string]interface{}{"Status": http.StatusOK, "Message": "Server is running!"}
+		json.NewEncoder(w).Encode(response)
 	})
 	return r
 }
